af13a6ec0fc6994fc86375bf30c8578ad2d713a8
WentOutForAWalk <dobysdanil@gmail.com>
2026-03-28 04:31:33 +0400

    Added support for controllers

--- SimpleNetCore/Program.cs
+++ SimpleNetCore/Program.cs
@@ -1,14 +1,28 @@
 using SimpleNetCore.Data;
 using SimpleNetCore.Endpoints;
+using SimpleNetCore.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Swagger ..
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Services
+builder.Services.AddSingleton<MemoryStorageService>();
+builder.Services.AddTransient<ChannelService>();
+builder.Services.AddTransient<MessageService>();
+
+// Controllers
+builder.Services.AddControllers();
 
 builder.AddSimpleNetCoreDb();
 
 var app = builder.Build();
+
+app.MapControllers();
+
+
 
 
 if (app.Environment.IsDevelopment())
