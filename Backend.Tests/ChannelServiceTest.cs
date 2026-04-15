209f72cc93769b39fcd8cef0606c479e21e7b3da
WentOutForAWalk <dobysdanil@gmail.com>
2026-04-15 23:26:08 +0400

    test fix

--- Backend.Tests/ChannelServiceTest.cs
+++ Backend.Tests/ChannelServiceTest.cs
@@ -14,16 +14,18 @@ public class ChannelServiceTest
         var options = new DbContextOptionsBuilder<DataContext>()
             .UseInMemoryDatabase("SimpleDb").Options;
         var context = new DataContext(options);
+        context.Database.EnsureDeleted();
+
 
         var userMock = new Mock<IUserContextService>();
         userMock.Setup(x => x.GetUserId()).Returns(Guid.Empty.ToString());
 
         var service = new ChannelService(context, userMock.Object);
 
-        var result = await service.AddChannelAsync(new ChannelRequest("Yolloooohohohoho"));
+        var result = await service.AddChannelAsync(new ChannelRequest("1"));
         
         Assert.True(result.IsSuccess);
-        Assert.Equal(result.Data.ChannelName)
+        Assert.Equal("1", result.Data!.ChannelName);
 
         Assert.Equal(1, context.Channels.Count());
     }
